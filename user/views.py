b44ee408f23b0054b83226d8ee45e36adc83d7d0
Mykola Totsevych <totsevich02@gmail.com>
2026-01-12 20:18:05 +0100

    add user creation and management views

--- user/views.py
+++ user/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from user.models import User
+from user.serializers import UserSerializer
+
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class UserManageView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self) -> User:
+        return self.request.user
