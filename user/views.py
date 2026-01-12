803217b1ffa4760928ce15b8d1557ff8ece9bd0c
Mykola Totsevych <totsevich02@gmail.com>
2026-01-12 21:24:15 +0100

    add permission_classes for UserCreateView and UserManageView

--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from user.models import User
 from user.serializers import UserSerializer
@@ -6,10 +7,12 @@ from user.serializers import UserSerializer
 
 class UserCreateView(generics.CreateAPIView):
     serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
 
 
 class UserManageView(generics.RetrieveUpdateAPIView):
     serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
 
     def get_object(self) -> User:
         return self.request.user
